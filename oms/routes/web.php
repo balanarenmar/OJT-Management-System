eb36f8518a3cabc786e95c49fab4221f90a43212
RenmarB <renmarfrancia.balana@bicol-u.edu.ph>
2023-06-27 02:51:39 +0800

    Added Prototype Blog Posts

--- oms/routes/web.php
+++ oms/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// INITIAL VIEW.
+//all listings
 Route::get('/', function () {
-    return view('welcome');
+    //second argument can be an array containing any values
+    return view('welcome', [
+        'heading' => 'Hello World',
+        'blog_posts' => BlogPost::all() //retrieves all blog posts from the model
+    ]);
+});
+
+
+Route::get('/blogpost/{id}', function ($id) {
+    return view('blogpost', [
+        'post' => BlogPost::find($id) //retrieves one blog post from the model
+    ]);
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>')
+        ->header('Content-Type', 'text/plain')
+        ->header('foor', 'bar')
+        ->cookie('name', 'John Doe', 3600);
+});
+
+
+Route::get('/posts/{id}', function($id) {
+    //ddd($id);                              //debugging
+    return response('Post ' . $id);
+    
+    //return 'This is post number ' . $id;
+})->where('id', '[0-9]+');
+
+//EXAMPLE request for routing using link 
+Route::get('/search', function(Request $request) {
+    
+    return($request->name . ' ' . $request->city);
+    dd($request);
 });
